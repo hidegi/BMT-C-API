662aa6f17de151f90b235f9019e15bf489377061
hidegi <nachv2010@gmail.com>
2026-08-24 16:32:16 +0200

    Updated .gitignore

---
+++ include/platform.h
@@ -0,0 +1,54 @@
+/****************************************************************************
+ * Copyright (c) 2024 Hidegi
+ *
+ * This software is provided ‘as-is’, without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source
+ * distribution.
+ ****************************************************************************/
+#ifndef BMTPLATFORM_H
+#define BMTPLATFORM_H
+
+#define BMTVERSION_MAJOR 2
+#define BMTVERSION_MINOR 0
+#define BMTVERSION_PATCH 2
+
+#define BMTHAVE_BST_MAJOR_INCLINED
+#define SP_STATIC_BUILD
+/* #undef SP_PLATFORM_WINDOWS */
+/* #undef SP_PLATFORM_MACOS */
+#define SP_PLATFORM_LINUX
+
+#ifndef SP_STATIC_BUILD
+	#if defined(SP_PLATFORM_WINDOWS)
+		#define SP_API __declspec(dllexport)
+	#else
+		#define SP_API __attribute__((visibility("default")))
+	#endif
+#else
+	#define SP_API
+#endif
+
+#if defined(__clang__)
+#define SP_COMPILER_CLANG
+#elif defined(__GNUC__) || defined(__GNUC__)
+#define SP_COMPILER_GNUC
+#elif defined(_MSC_VER)
+#define SP_COMPILER_MSC
+#endif
+
+#endif
